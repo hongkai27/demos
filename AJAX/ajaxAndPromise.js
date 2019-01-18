9b2f9f2be6a178a536102e5261381a748b6e5a1a
hongkai <952817116@qq.com>
2019-01-18 16:37:16 +0800

    AJAX基础学习

--- AJAX/ajaxAndPromise.js
+++ AJAX/ajaxAndPromise.js
@@ -1,19 +1,19 @@
 封装AJAX
 window.jQuery.ajax = function({url,method,body,haeaders}){
-    return new Promise(function(resolve,reject){
-        let request = new XMLHttpRequest
-        request.open(method,url)
-        for(let key in headers){
+    return new Promise(function(resolve,reject){   //固定套路，背下来
+        let request = new XMLHttpRequest    //1、新建需求对象
+        request.open(method,url)  //2、配置request，设置和服务器交互的信息
+        for(let key in headers){  //3、需要设置header的话加上
             let value = headers[key]
             request.setRequestheader(key,value)
         }
-        request.send()
-        request.onreadystatechange = ()=>{
-            if(request.readyState === 4){
-                if(request.status >=200 && request.status < 300){
-                    resolve.call(undefined,request.responseText)
+        request.send()  //4、发送请求
+        request.onreadystatechange = ()=>{//解析响应信息
+            if(request.readyState === 4){//步骤执行结束
+                if(request.status >=200 && request.status < 300){//判断状态码
+                    resolve.call(undefined,request.responseText)//成功函数
                 }else if(request.status >= 400){
-                    reject.call(undefined,request)
+                    reject.call(undefined,request)//失败函数
                 }
             }
         }
@@ -22,6 +22,19 @@ window.jQuery.ajax = function({url,method,body,haeaders}){
 
 
 客户端添加事件
+/*简化版 */
+Button.addEventListener('click', () => {
+    let obj = {
+        url: '/xxx',
+        method: 'get',
+        headers: {
+            'content-type': 'html/javascript',
+            'ccc': 'bbb'
+        },
+        window,jQuery,ajax(obj)
+})
+
+/*精装版 */
 Button.addEventListener('click',()=>{
     let promise = window.jQuery.ajax({
         url:'/xxx',
