01e354a9249dcdce81aaaff089c5e2ee332b3ba0
hongkai <952817116@qq.com>
2019-01-28 17:06:45 +0800

    跳动的简历

--- jumpCartoon/cartoon.js
+++ jumpCartoon/cartoon.js
@@ -1,5 +1,4 @@
-
-let result = `/*
+let text1 = `/*
 * 面试官你好，我是洪凯
 * 首先我想简单的介绍我自己
 * 只是以文字介绍太单调了
@@ -10,11 +9,10 @@ let result = `/*
     transition:all 1s;
 }
 html{
-    background:rgb(222,222,222);
+    background:#fff8dc;
 }
 #pre{
     padding:10px;
-    border:1px solid red;
 }
 /*现在我需要使一些代码高亮*/
 .token.selector{
@@ -33,45 +31,103 @@ html{
 /*下面开始进入正题，介绍一下自己吧 */
 /* 首先，我需要一张白纸 */
 `
-writeCode(result)
+
+let text2 = `
+#pre{
+    position:fixed;
+    left:0;
+    height:100%;
+    width:50%;
+}
+#paper{
+    position:fixed;
+    right:0;
+    width:50%;
+    height:100%;
+    background:grey;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+}
+#paper #content{
+    background:white;
+    width:98%;
+    height:98%;
+}
+/*下面我们干脆把MARKDOWN内容也变为html模式吧*/
+`
+let note = `
+#自我介绍：
+
+姓名：洪 凯
+意向岗位：前端开发工程师
+已熟练掌握的技术栈：Javascript,ES6,Bootstrap,html,css,git,webpack等
+熟悉后端语言:python
+喜欢敏捷开发，研究新技术
+关注前端发展潮流，勇于尝试使用新技术
+
+#项目介绍：
+
+1、类似苹果风格轮播
+2、跳动的简历
+3、CANVAS画板
+4、网易云音乐
+5、VUE造轮子
+
+#个人信息：
+
+联系方式：15051885755
+E-mail：952817116@qq.com
+gitHub: github.com/hongkai27
+weibo: 沉默的咖啡壶
+
+
+
+`
+writeCode('', text1, () => {
+    createPaper(() => {
+        writeCode(text1, text2,()=>{
+            writeMarkdown(note)
+        })
+    })
+})
 
 /*................................ */
-function writeCode(code,fn) {
+function writeCode(before, code, fn) {//before是前缀内容
     let preCode = document.querySelector('#pre')
     let n = 0
-    let id = setInterval(() => {  //preCode.innerHTML通过把code在高亮库加工再赋值
-        preCode.innerHTML = Prism.highlight(code.substring(0, n), Prism.languages.css)
-        preStyle.innerHTML = code.substring(0, n)
-        n += 1                               
-        if (n >= code.length) {
-            window.clearInterval(id)
-            fn2.call()
+    let time1 = setInterval(() => {  //preCode.innerHTML通过把code在高亮库加工再赋值
+        n += 1
+        preStyle.innerHTML = before + code.substring(0, n)
+        preCode.innerHTML = Prism.highlight(before + code.substring(0, n), Prism.languages.css)
+        preCode.scrollTop = preCode.scrollHeight//没有滚动条时，代码向下滚动，防止代码看不见
+        if (n > code.length) {
+            window.clearInterval(time1)
+            fn.call()
         }
-    }, 10)
+    },10)
 }
 
-
-function fn2() {
-    var paper = document.createElement('div')
+function createPaper(fn) {
+    let paper = document.createElement('div')
     paper.id = 'paper'
     document.body.appendChild(paper)
+    let content = document.createElement('pre')
+    content.id = 'content'
+    paper.appendChild(content)
+    fn.call()
 }
-function fn3(x) {
-    var result = `#paper{
-        width:600px;
-        height:800px;
-        background:grey;
-    }
-    `
-    var n = 0
-    var id = setInterval(() => {
-        //直接加x：result是因为上面'逐步出现'这个动画已经执行过，可以让result直接出现了
-        preStyle.innerHTML = x + result.substring(0, n)
-        pre.innerHTML = x + result.substring(0, n)
-        pre.innerHTML = Prism.highlight(pre.innerHTML, Prism.languages.css);
+
+function writeMarkdown(note,fn){
+    let content = document.querySelector('#paper > #content')
+    let n = 0
+    let time = setInterval(() => {
         n += 1
-        if (n >= result.length) {
-            window.clearInterval(id)
+        content.innerHTML = note.substring(0, n)
+        content.scrollTop = content.scrollHeight
+        if (n > note.length) {
+            window.clearInterval(time)
+            fn.call()
         }
-    }, 10)
+    },10)
 }
