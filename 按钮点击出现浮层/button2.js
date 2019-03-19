f68c9fa4f986a70b5c63858592e2ab2e71bc4503
hongkai <952817116@qq.com>
2019-03-19 19:44:43 +0800

    x

--- 按钮点击出现浮层/button2.js
+++ 按钮点击出现浮层/button2.js
@@ -1,24 +1,33 @@
 /*
 $(clickme2).on('click', function () {
     $(pop2).show()
-    setTimeout((function(){
+    console.log('1')
+    setTimeout((function () {
+        console.log('2')
         $(document).one('click', function () {
             $(pop2).hide()
+            console.log('3')
         })
-    }),0)
+        $(document).one('mousemove', function () {
+            let a = document.createElement('a')
+            a.innerHTML = 'baidu'
+            wrapper2.appendChild(a)
+            console.log('4')
+        })
+        console.log('5')
+    }), 0)
 })
 */
 
 
 
-
 $(clickme2).on('click', function () {
-    $(pop2).toggle()         /* toggle相当于一个开关，可以切换状态*/
+    $(pop2).toggle()
         $(document).one('click', function () {
             $(pop2).hide()
         })
 })
 $(wrapper2).on('click', function (e) {
-    e.stopPropagation() 
+    e.stopPropagation()
 })
 
